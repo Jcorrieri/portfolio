2c9bca31e511e9e283c73d7207ac0b6c2d951757
Jcorrieri <jacomocorrieri@gmail.com>
2025-06-17 23:28:10 -0400

    restructure project directory, update local scripts, add new article

--- js/study.js
+++ js/study.js
@@ -1,5 +1,4 @@
 document.addEventListener('DOMContentLoaded', () => {
-   const galleryDiv = document.getElementById('gallery-div');
    const submitBtn = document.getElementById('search-button');
    const textfield = document.getElementById('gallery-input');
    const tagSelector = document.getElementById('tag-selector');
@@ -7,24 +6,23 @@ document.addEventListener('DOMContentLoaded', () => {
 
    submitBtn?.addEventListener('click', () => {
        noResult.classList.add('hidden');
-       let query = textfield.value.trim().toUpperCase();
-       let tag = tagSelector.value.replace('-', ' ');
+       let query = textfield.value.trim().toLowerCase();
+       let tag = tagSelector.value;
 
        let visibleCount = 0;
-       let galleryItems = Array.from(galleryDiv.children).slice(0, -1); // Exclude last child
+       document.querySelectorAll('a.article-link').forEach(link => {
+           const title = link.dataset.title;
+           const keywords = link.dataset.keywords;
+           const tags = link.dataset.categories;
 
-       galleryItems.forEach(galleryItem => {
-           let itemTitle = galleryItem.querySelector('h2')?.textContent.trim().toUpperCase() || '';
-           let itemTags = Array.from(galleryItem.querySelectorAll('li')).map(li => li.textContent);
-
-           let matchesTag = tag === "All" || itemTags.includes(tag);
-           let matchesQuery = itemTitle.includes(query);
+           let matchesTag = tag === "all" || tags.includes(tag);
+           let matchesQuery = title.includes(query) || keywords.includes(query);
 
            if (matchesTag && matchesQuery) {
-               galleryItem.classList.remove('hidden');
+               link.classList.remove('hidden');
                visibleCount++;
            } else {
-               galleryItem.classList.add('hidden');
+               link.classList.add('hidden');
            }
        });
 
